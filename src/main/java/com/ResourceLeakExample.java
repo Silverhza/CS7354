eb0836b7aa8650985ce4c4f3de4d5a3ce781c83e
MasterFlower <1260297455@qq.com>
2025-10-17 14:53:52 +0800

    Update ResourceLeakExample.java

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -33,12 +33,13 @@ public class ResourceLeakExample {
    //     // 不需要显式关闭BufferedReader，try-with-resources会自动处理
    // }
 	
-	public static void main(String[] args) {
-		String password;
-        password = "12345"; 
-        System.out.println(password);
-    }
+	// public static void main(String[] args) {
+	// 	String password;
+ //        password = "12345"; 
+ //        System.out.println(password);
+ //    }
 
 }
+
 
 
